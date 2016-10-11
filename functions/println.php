56de2a87be8e7e6fc689a01c97485cd5a29fa884
assertchris <cgpitt@gmail.com>
2016-10-12 11:49:32 +1300

    Trying a different approach to 5.5 compat...

--- functions/println.php
+++ functions/println.php
@@ -1,36 +1,35 @@
 <?php
 
 if (!function_exists("println")) {
+    if (PHP_VERSION >= "5.6") {
+        eval('
+            function println(...$strings) {
+                foreach ($strings as $string) {
+                    print $string;
 
-    if (PHP_VERSION_ID >= 50600) {
+                    if (php_sapi_name() !== "cli") {
+                        print "<br>";
+                    }
 
-        function println(...$strings) {
-            foreach ($strings as $string) {
-                print $string;
-
-                if (php_sapi_name() !== "cli") {
-                    print "<br>";
+                    print PHP_EOL;
                 }
-
-                print PHP_EOL;
             }
-        }
-
+        ');
     } else {
+        eval('
+            function println() {
+                $strings = func_get_args();
 
-        function println() {
-            $strings = func_get_args();
+                foreach ($strings as $string) {
+                    print $string;
 
-            foreach ($strings as $string) {
-                print $string;
+                    if (php_sapi_name() !== "cli") {
+                        print "<br>";
+                    }
 
-                if (php_sapi_name() !== "cli") {
-                    print "<br>";
+                    print PHP_EOL;
                 }
-
-                print PHP_EOL;
             }
-        }
-
+        ');
     }
 }
